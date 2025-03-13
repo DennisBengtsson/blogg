99c37563ff4302928da5f562f43ceb440fd4d899
Dennis Bengtsson <dennisrmgbengtsson@gmail.com>
2025-03-13 22:29:02 +0100

    Nähä??

--- js/post-navigation.js
+++ js/post-navigation.js
@@ -3,7 +3,7 @@
 // Funktion för att hämta blogginlägg från JSON-filen
 async function getBlogPosts() {
     try {
-        const response = await fetch('./json/blog_posts.json');
+        const response = await fetch('../json/blog_posts.json');
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
